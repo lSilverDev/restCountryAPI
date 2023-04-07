2bbae0001b32aa79db80fe96c7c8d8227750bfdc
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-04-07 11:03:49 -0300

    routing and style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { FlagDetailsComponent } from './components/flag-details/flag-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'flagDetail',
+    component: FlagDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
